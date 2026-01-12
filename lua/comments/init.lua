4958c9ba086e01b5bdd77955f7f0ca79d9872002
Luis Roa <luis.roa@feverup.com>
2026-01-12 17:48:01 +0100

    refactor(render): improve floating window

--- lua/comments/init.lua
+++ lua/comments/init.lua
@@ -95,15 +95,34 @@ M.show_comment = function()
   end
 
   local t, width, height = render.render_thread(thread, MAX_COL)
+  local buf = vim.api.nvim_create_buf(false, true)
 
-  vim.lsp.util.open_floating_preview(vim.tbl_flatten(t), 'markdown',
-    {
-      width = width,
-      height = height - 1,
-      border = 'rounded',
-      x_offset = 10,
-      relative = 'cursor'
-    })
+  for _, comment in ipairs(t) do
+    vim.api.nvim_buf_set_lines(buf, -1, -1, false, comment)
+  end
+
+  if vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] == "" then
+    vim.api.nvim_buf_set_lines(buf, 0, 1, false, {})
+  end
+
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'markdown')
+  vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = 'cursor',
+    row = 1,
+    col = 0,
+    width = width,
+    height = height,
+    style = 'minimal',
+    border = 'rounded'
+  })
+
+  vim.api.nvim_win_set_option(win, 'wrap', true)
+  vim.api.nvim_win_set_option(win, 'linebreak', true)
+  vim.api.nvim_win_set_option(win, 'breakindent', true)
+  vim.api.nvim_win_set_option(win, 'conceallevel', 2)
+  vim.api.nvim_win_set_option(win, 'concealcursor', 'nc')
 end
 
 
